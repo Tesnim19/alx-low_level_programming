dfee07199f136ab5d8a6124a6586f1602417211c
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-13 17:33:24 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *prints the phrase
+ *
+ *Description: print phrase
+ *Return: zero if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
