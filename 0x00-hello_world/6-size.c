d35804691ebda1ac6da9a8bbfbf0b90b169868fe
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-13 17:58:22 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  * Description print size of
  * data types
- *
- * Return : 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwie
  */
 int main(void)
 {
