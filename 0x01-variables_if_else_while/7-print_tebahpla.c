9a4277faad55f7f9da271094c84d8bbde14b02ac
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-17 12:33:43 +0000

    added task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * description print lowercase alphabet
+ *
+ * Return: Always value 0 if success
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 122; i <= 98; i++)
+	{
+		if (i == 98)
+		{
+			putchar('\n');
+			break;
+		}
+		c = (char)i;
+		putchar(c);
+	}
+	return (0);
+}
+
