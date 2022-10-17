32f4fe3edfdce5886e8f770496d5a91fec4faa94
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-17 13:39:20 +0000

    added task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 30; i <= 39; i++)
+	{
+		putchar(i);
+	}
+	for (i = 61; i <= 66; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
