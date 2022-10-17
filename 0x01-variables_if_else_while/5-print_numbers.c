a338388218a14f6f9949bac83f8b24be0339465e
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-17 12:09:26 +0000

    added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print all 1 digit number
+ *
+ * Return - Always 0 if sucessfule
+ *
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf(n);
+		n++;
+	}
+	printf('\n');
+	return (0);
+}
