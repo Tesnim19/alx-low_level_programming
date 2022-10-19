34a56a0ad7465992c96dcb8eb6a18b505b91809c
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-19 23:19:13 +0000

    added task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,28 +15,26 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			if (n < 0 || n > 15)
-			{
 				break;
-				k = i * j;
-				if (j == 0)
-				{
-					printf("%d", k);
-				}
-				if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					printf("%d", k);
-				}
-				else if (k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					printf("%d", k);
-				}
+			k = i * j;
+			if (j == 0)
+			{
+				printf("%d", k);
 			}
-			_putchar('\n');
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				printf("%d", k);
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				printf("%d", k);
+			}
 		}
+		_putchar('\n');
 	}
 }
