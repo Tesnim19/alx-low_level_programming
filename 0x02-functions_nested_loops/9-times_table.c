6d0185f78576c2178da841abe12dc73fd9430b39
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-19 22:14:35 +0000

    added task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,18 @@ void times_table(void)
 			{
 				_putchar(k +'0');
 			}
-			if (k >= 10 && j != 0)
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + k);
 			}
 		}
 		_putchar('\n');
