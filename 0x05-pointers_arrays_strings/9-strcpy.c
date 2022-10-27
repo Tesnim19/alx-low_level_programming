a74f62bad28743e376ce2a085a1e1f23810658a7
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-27 21:05:34 +0000

    added task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,6 +9,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
 	return (dest);
 }
