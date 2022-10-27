b8b361868d3b8e84e8ccab97d7d3117736e65bcd
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-27 19:24:37 +0000

    added task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,18 @@
  */
 void print_rev(char *s)
 {
-	int len = _strlen(s);
+	int len = 0;
 
 	while (len >= 0)
 	{
+		if (s[len] == '\0')
+			break;
+		len++;
+	}
+
+	while (--len >= 0)
+	{
 		_putchar(s[len]);
-		len--;
 	}
 	_putchar('\n');
 }
