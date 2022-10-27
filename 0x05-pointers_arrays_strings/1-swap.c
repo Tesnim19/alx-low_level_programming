788c361fd95ca7bb5bd04048c665270d8241c93a
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-27 17:02:47 +0000

    added task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,14 +2,15 @@
 
 /**
  * swap_int - swaps two intgers using ponter
- * @a - input integer
- * @b - input integer
+ * @a: input integer
+ * @b: input integer
  *
  * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
