6e3c6eba43f028d606dc399dcedd6156e403cfb5
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-27 21:03:03 +0000

    added task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to by src
+ * @dest: input char
+ * @src: input char
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
