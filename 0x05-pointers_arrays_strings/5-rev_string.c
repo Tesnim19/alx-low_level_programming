6210aacfa5cfb2e5910ded0dc6467f5a111034ec
Tesnim19 <tesnimabdi67@gmail.com>
2022-10-27 19:52:37 +0000

    task 5 added

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,24 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, j = 0;
-	char *str;
+	int count = 0, i, j;
+	char *str, temp;
 
-	while (i >= 0)
+	while (count >= 0)
 	{
-		if (s[i] == '\0')
+		if (s[count] == '\0')
 			break;
-		i++;
+		count++;
 	}
-	str = s + i;
-	*str = *s;
+	str = s;
 
-	while (--i >= 0)
+	for (i = 0; i < (count - 1); i++)
 	{
-		str[j] = s[i];
-		j++;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
-	*s = *str;
 }
