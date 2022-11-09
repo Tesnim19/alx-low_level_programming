ad51eda63f7a531e40115145a9a6e1d0b75aac98
Tesnim19 <tesnimabdi67@gmail.com>
2022-11-09 13:12:36 -0800

    task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
+ * main - prints name of program.
+ * @argc: number of command line arguments
+ * @argv: An array containing the program command line argument
  *
- * * main - prints name of program.
+ * Return: Always success
  *
- * * @argc: number of command line arguments
- *
- * * @argv: An array containing the program command line arguments
- *
- * *
- *
- * * Return: Always success.
- *
- * */
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
